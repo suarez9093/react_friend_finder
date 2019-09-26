f445e9cef816ee6eca53fed89387a9a6ca8f94a0
Alex <suarez9093@hotmail.com>
2019-09-25 21:20:31 -0600

    creating a homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Home from "./pages/Home"
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Books} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} />
           <Route component={NoMatch} />
