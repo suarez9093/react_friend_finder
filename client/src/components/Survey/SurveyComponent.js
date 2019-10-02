ee1f742dbb8964744da93e46fe32757b5312378f
Alex <suarez9093@hotmail.com>
2019-10-01 21:52:29 -0600

    got values to push to state. Need to figure out how to save them as an array

--- client/src/components/Survey/SurveyComponent.js
+++ client/src/components/Survey/SurveyComponent.js
@@ -3,18 +3,17 @@ import React from "react";
 function SurveyComponent(props) {
     return (
         <div>
-
             <div>
                 <h3>Question 1</h3>
                 <p>You find it easy to get along with others</p>
-                <select 
+                <select
                     data-placeholder="" 
-                    className="chosen-select" 
+                    class="chosen-select" 
                     id="q1"
                     name="answers"
-                    value={props.data.answers}
                     onChange={props.handleChange}
-                    >
+                    value={props.data.answers}
+                 >
                     <option selected>---</option>
                     <option value="1">1 (Strongly Disagree)</option>
                     <option value="2">2</option>
@@ -24,11 +23,17 @@ function SurveyComponent(props) {
                 </select>
             </div>
             <br />
-
             <div>
                 <h3>Question 2</h3>
                 <p>You have hobbies outside of work</p>
-                <select data-placeholder="" className="chosen-select" id="q2">
+                <select
+                    data-placeholder="" 
+                    class="chosen-select" 
+                    id="q2"
+                    name="answers"
+                    onChange={props.handleChange}
+                    value={props.data.answers}
+                 >
                     <option selected>---</option>
                     <option value="1">1 (Strongly Disagree)</option>
                     <option value="2">2</option>
@@ -37,119 +42,6 @@ function SurveyComponent(props) {
                     <option value="5">5 (Strongly Agree)</option>
                 </select>
             </div>
-            <br />
-
-            <div>
-                <h3>Question 3</h3>
-                <p>You are close with your family</p>
-                <select data-placeholder="" className="chosen-select" id="q3">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 4</h3>
-                <p>You are confortable meeting new people</p>
-                <select data-placeholder="" className="chosen-select" id="q4">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 5</h3>
-                <p>People would call you an extrovert</p>
-                <select data-placeholder="" className="chosen-select" id="q5">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 6</h3>
-                <p>You have goals</p>
-                <select data-placeholder="" className="chosen-select" id="q6">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 7</h3>
-                <p>You are physically active</p>
-                <select data-placeholder="" className="chosen-select" id="q7">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 8</h3>
-                <p>You like to eat sushi</p>
-                <select data-placeholder="" className="chosen-select" id="q8">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-            <div>
-                <h3>Question 9</h3>
-                <p>Others speak highly of you</p>
-                <select data-placeholder="" className="chosen-select" id="q9">
-                    <option selected>---</option>
-                    <option value="1">1 (Strongly Disagree)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5 (Strongly Agree)</option>
-                </select>
-            </div>
-            <br />
-
-                <div>
-                    <h3>Question 10</h3>
-                    <p>You believe you can do anything you set your mind to</p>
-                    <select data-placeholder="" className="chosen-select" id="q10">
-                        <option selected>---</option>
-                        <option value="1">1 (Strongly Disagree)</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5 (Strongly Agree)</option>
-                    </select>
-                </div>
-            
         </div>
             )
         }
--- client/src/components/Survey/SurveyContainer.js
+++ client/src/components/Survey/SurveyContainer.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import SurveyComponent from "./SurveyComponent";
 
 class SurveyContainer extends Component {
-    state = {
-        answers: []
-    }
+       state = {
+            answers: []
+        }
 
-    handleChange(event) {
-        const { name, value } = event.target
-        this.setState({[name] : value})
+    handleChange = (event) => {
+        const { name, value, type } = event.target
+        this.setState({ [name]: value })
     }
-    render () {
+    render() {
         return (
-            <SurveyComponent 
-                handleChange={this.handleChange}
-                data={this.state}
-            />
+            <div>
+                <SurveyComponent
+                    handleChange={this.handleChange}
+                    data={this.state}
+                />
+                <p>{console.log(this.state.answers)}</p>
+            </div>
         )
     }
 }
